f7466137bb2e44c4a32d869012bb8f5b5567c525
RafaelW <rabe.wortmann@gmail.com>
2023-01-20 19:47:43 +0100

    added vuestic

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
+	<head>
+		<link
+			href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap"
+			rel="stylesheet"
+			>
+		<link
+			href="https://fonts.googleapis.com/icon?family=Material+Icons"
+			rel="stylesheet"
+			>
+	</head>
 	<RouterView />
 </template>
 
@@ -13,5 +23,5 @@ body,
 	min-height: 100vh;
 	height: 100%;
 	width: 100%;
-}
+};
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
+import{createVuestic} from "vuestic-ui";
 import router from "./router";
 
+import "vuestic-ui/css"
 import "./main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(createVuestic())
 
 app.mount("#app");
